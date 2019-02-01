ab361ffd17232b55b1707446bf4d1eb14d9e23dd
Alex Eden <alexandereden91@gmail.com>
2019-01-31 23:12:40 -0600

    Finish converting Effect to TS

--- src/effects/Effect.ts
+++ src/effects/Effect.ts
@@ -1,120 +1,87 @@
-import { BlendFunction } from './blending/BlendFunction.js';
-import { BlendMode } from './blending/BlendMode.js';
-import { Initializable, Resizable, Disposable } from 'core/index.js';
+import { Texture, WebGLRenderer, WebGLRenderTarget } from 'three';
+import { Initializable, Resizable, Disposable } from '../core';
+import { BlendFunction, BlendMode } from './blending';
+import {
+  EffectAttribute,
+  EffectName,
+  EffectOptions,
+  FragmentShader,
+  PreprocessorMacros,
+  ShaderUniforms,
+  VertexShader,
+  WebGLExtension,
+} from './lib';
+
 
 /**
  * An abstract effect.
  * Effects can be combined using the {@link EffectPass}.
  */
-
-export class Effect implements Initializable, Resizable, Disposable {
+export abstract class Effect implements Initializable, Resizable, Disposable {
+  [k: string]: any;
 
   /**
-   * Constructs a new effect.
+   * The effect attributes.
    *
-   * @param {String} name - The name of this effect. Doesn't have to be unique.
-   * @param {String} fragmentShader - The fragment shader. This shader is required.
-   * @param {Object} [options] - Additional options.
-   * @param {EffectAttribute} [options.attributes=EffectAttribute.NONE] - The effect attributes that determine the execution priority and resource requirements.
-   * @param {BlendFunction} [options.blendFunction=BlendFunction.SCREEN] - The blend function of this effect.
-   * @param {Map<String, String>} [options.defines] - Custom preprocessor macro definitions. Keys are names and values are code.
-   * @param {Map<String, Uniform>} [options.uniforms] - Custom shader uniforms. Keys are names and values are uniforms.
-   * @param {Set<WebGLExtension>} [options.extensions] - WebGL extensions.
-   * @param {String} [options.vertexShader=null] - The vertex shader. Most effects don't need one.
+   * Effects that have the same attributes will be executed in the order in
+   * which they were registered. Some attributes imply a higher priority.
    */
+  attributes: EffectAttribute;
+  vertexShader: VertexShader | null = null;
 
-  constructor(name, fragmentShader, options = {}) {
+  /**
+   * Preprocessor macro definitions.
+   * You'll need to call {@link EffectPass#recompile} after changing a macro.
+   */
+  defines: PreprocessorMacros;
 
-    const settings = {
+  /**
+   * Shader uniforms.
+   * You may freely modify the values of these uniforms at runtime. However,
+   * uniforms must not be removed or added after the effect was created.
+   */
+  uniforms: ShaderUniforms;
+
+  /**
+   * WebGL extensions that are required by this effect.
+   * You'll need to call {@link EffectPass#recompile} after adding or removing
+   * an extension.
+   */
+  extensions: Set<WebGLExtension>;
+
+  /**
+   * The blend mode of this effect.
+   *
+   * The result of this effect will be blended with the result of the previous
+   * effect using this blend mode.
+   *
+   * Feel free to adjust the opacity of the blend mode at runtime. However,
+   * you'll need to call {@link EffectPass#recompile} if you change the blend
+   * function.
+   */
+  blendMode: BlendMode;
+
+  constructor(
+    readonly name: EffectName,
+    readonly fragmentShader: FragmentShader,
+    partialOptions: Partial<EffectOptions> = { }
+  ) {
+    const settings: EffectOptions = {
       attributes: EffectAttribute.NONE,
       blendFunction: BlendFunction.SCREEN,
       defines: new Map(),
       uniforms: new Map(),
-      extensions: null,
-      vertexShader: null,                             ...options};
-
-    /**
-     * The name of this effect.
-     *
-     * @type {String}
-     */
-
-    this.name = name;
-
-    /**
-     * The effect attributes.
-     *
-     * Effects that have the same attributes will be executed in the order in
-     * which they were registered. Some attributes imply a higher priority.
-     *
-     * @type {EffectAttribute}
-     */
+      extensions: new Set(),
+      vertexShader: null,
+      ...partialOptions,
+    };
 
     this.attributes = settings.attributes;
-
-    /**
-     * The fragment shader.
-     *
-     * @type {String}
-     */
-
-    this.fragmentShader = fragmentShader;
-
-    /**
-     * The vertex shader.
-     *
-     * @type {String}
-     */
-
     this.vertexShader = settings.vertexShader;
-
-    /**
-     * Preprocessor macro definitions.
-     *
-     * You'll need to call {@link EffectPass#recompile} after changing a macro.
-     *
-     * @type {Map<String, String>}
-     */
-
     this.defines = settings.defines;
-
-    /**
-     * Shader uniforms.
-     *
-     * You may freely modify the values of these uniforms at runtime. However,
-     * uniforms must not be removed or added after the effect was created.
-     *
-     * @type {Map<String, Uniform>}
-     */
-
     this.uniforms = settings.uniforms;
-
-    /**
-     * WebGL extensions that are required by this effect.
-     *
-     * You'll need to call {@link EffectPass#recompile} after adding or removing
-     * an extension.
-     *
-     * @type {Set<WebGLExtension>}
-     */
-
     this.extensions = settings.extensions;
-
-    /**
-     * The blend mode of this effect.
-     *
-     * The result of this effect will be blended with the result of the previous
-     * effect using this blend mode.
-     *
-     * Feel free to adjust the opacity of the blend mode at runtime. However,
-     * you'll need to call {@link EffectPass#recompile} if you change the blend
-     * function.
-     *
-     * @type {BlendMode}
-     */
-
     this.blendMode = new BlendMode(settings.blendFunction);
-
   }
 
   /**
@@ -122,12 +89,11 @@ export class Effect implements Initializable, Resizable, Disposable {
    *
    * You may override this method if your effect requires direct access to the
    * depth texture that is bound to the associated {@link EffectPass}.
-   *
-   * @param {Texture} depthTexture - A depth texture.
-   * @param {Number} [depthPacking=0] - The depth packing.
    */
-
-  setDepthTexture(depthTexture, depthPacking = 0) {}
+  setDepthTexture(
+    depthTexture: Texture,
+    depthPacking = 0
+  ) { }
 
   /**
    * Updates the effect by performing supporting operations.
@@ -138,12 +104,15 @@ export class Effect implements Initializable, Resizable, Disposable {
    * You may override this method if you need to render additional off-screen
    * textures or update custom uniforms.
    *
-   * @param {WebGLRenderer} renderer - The renderer.
-   * @param {WebGLRenderTarget} inputBuffer - A frame buffer that contains the result of the previous pass.
-   * @param {Number} [delta] - The time between the last frame and the current one in seconds.
+   * @param renderer - The renderer.
+   * @param inputBuffer - A frame buffer that contains the result of the previous pass.
+   * @param delta - The time between the last frame and the current one in seconds.
    */
-
-  update(renderer, inputBuffer, delta) {}
+  update(
+    renderer: WebGLRenderer,
+    inputBuffer: WebGLRenderTarget,
+    delta: number = 0
+  ) { }
 
   /**
    * Updates the size of this effect.
@@ -153,13 +122,11 @@ export class Effect implements Initializable, Resizable, Disposable {
    *
    * The {@link EffectPass} calls this method before this effect is initialized
    * and every time its own size is updated.
-   *
-   * @param {Number} width - The width.
-   * @param {Number} height - The height.
-   * @example this.myRenderTarget.setSize(width, height);
    */
-
-  setSize(width, height) {}
+  setSize(
+    width: number,
+    height: number
+  ) { }
 
   /**
    * Performs initialization tasks.
@@ -174,12 +141,14 @@ export class Effect implements Initializable, Resizable, Disposable {
    * The {@link EffectPass} calls this method during its own initialization
    * which happens after the size has been set.
    *
-   * @param {WebGLRenderer} renderer - The renderer.
-   * @param {Boolean} alpha - Whether the renderer uses the alpha channel or not.
-   * @example if(!alpha) { this.myRenderTarget.texture.format = RGBFormat; }
+   * @param renderer - The renderer.
+   * @param alpha - Whether the renderer uses the alpha channel or not.
+   * @example if(!alpha) this.myRenderTarget.texture.format = RGBFormat;
    */
-
-  initialize(renderer, alpha) {}
+  initialize(
+    renderer: WebGLRenderer,
+    alpha: boolean
+  ) { }
 
   /**
    * Performs a shallow search for properties that define a dispose method and
@@ -193,60 +162,12 @@ export class Effect implements Initializable, Resizable, Disposable {
    * The {@link EffectPass} calls this method when it is being destroyed. Do not
    * call this method directly.
    */
-
   dispose() {
-
     for (const key of Object.keys(this)) {
-
       if (this[key] !== null && typeof this[key].dispose === 'function') {
-
         this[key].dispose();
         this[key] = null;
-
       }
-
     }
-
   }
-
 }
-
-/**
- * An enumeration of effect attributes.
- *
- * Attributes can be concatenated using the bitwise OR operator.
- *
- * @type {Object}
- * @property {Number} CONVOLUTION - Describes effects that fetch additional samples from the input buffer. There cannot be more than one effect with this attribute per {@link EffectPass}.
- * @property {Number} DEPTH - Describes effects that require a depth texture.
- * @property {Number} NONE - No attributes. Most effects don't need to specify any attributes.
- * @example
- * const attributes = EffectAttribute.CONVOLUTION | EffectAttribute.DEPTH;
- */
-
-export const EffectAttribute = {
-
-  CONVOLUTION: 2,
-  DEPTH: 1,
-  NONE: 0,
-
-};
-
-/**
- * An enumeration of WebGL extensions.
- *
- * @type {Object}
- * @property {String} DERIVATIVES - Enables derivatives by adding the functions dFdx, dFdy and fwidth.
- * @property {String} FRAG_DEPTH - Enables gl_FragDepthEXT to set a depth value of a fragment from within the fragment shader.
- * @property {String} DRAW_BUFFERS - Enables multiple render targets (MRT) support.
- * @property {String} SHADER_TEXTURE_LOD - Enables explicit control of texture LOD.
- */
-
-export const WebGLExtension = {
-
-  DERIVATIVES: 'derivatives',
-  FRAG_DEPTH: 'fragDepth',
-  DRAW_BUFFERS: 'drawBuffers',
-  SHADER_TEXTURE_LOD: 'shaderTextureLOD',
-
-};
