cd0564a79b2badec01688822a6782e128d405470
Alex Eden <alexandereden91@gmail.com>
2019-02-01 02:01:15 -0600

    I mean it's gotta work eventually amirite

--- webpack.config.js
+++ webpack.config.js
@@ -22,7 +22,9 @@ module.exports = {
 
 	output: {
 		filename: "[name].js",
-		path: path.resolve(__dirname, "dist")
+		path: path.resolve(__dirname, "dist"),
+		libraryTarget: "umd",
+		library: "postprocessing"
 	},
 
 	externals: {
