1faaafd18da4b8d26a2f679e1541f6e57810e4fe
Alex Eden <alexandereden91@gmail.com>
2019-02-01 00:48:11 -0600

    Start converting the last 3 passes needed for typed build

--- src/passes/index.ts
+++ src/passes/index.ts
@@ -4,13 +4,13 @@
  * @module postprocessing/passes
  */
 // export { BlurPass } from './BlurPass';
-// export { ClearMaskPass } from './ClearMaskPass';
+export { ClearMaskPass } from './ClearMaskPass';
 export { ClearPass } from './ClearPass';
 // export { DepthPass } from './DepthPass';
 // export { EffectPass } from './EffectPass';
-// export { MaskPass } from './MaskPass';
+export { MaskPass } from './MaskPass';
 // export { NormalPass } from './NormalPass';
 export { Pass } from './Pass';
 export { RenderPass } from './RenderPass';
 // export { SavePass } from './SavePass';
-// export { ShaderPass } from './ShaderPass';
+export { ShaderPass } from './ShaderPass';
