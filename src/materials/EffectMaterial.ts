5893c78ccb9c02b6b81e36abd61f49573abbee1a
Alex Eden <alexandereden91@gmail.com>
2019-02-01 22:13:38 -0600

    FInish converting EffectMaterial

--- src/materials/EffectMaterial.ts
+++ src/materials/EffectMaterial.ts
@@ -1,5 +1,6 @@
 import { PerspectiveCamera, ShaderMaterial, Uniform, Vector2 } from 'three';
-
+import { Resizable } from '../core';
+import { Section } from './lib';
 import fragmentTemplate from './glsl/effect/shader.frag';
 import vertexTemplate from './glsl/effect/shader.vert';
 
@@ -7,36 +8,30 @@ import vertexTemplate from './glsl/effect/shader.vert';
  * An effect material for compound shaders.
  *
  * This material supports dithering.
- *
- * @implements {Resizable}
  */
-
-export class EffectMaterial extends ShaderMaterial {
-
+export class EffectMaterial extends ShaderMaterial implements Resizable {
   /**
    * Constructs a new effect material.
    *
-   * @param {Map<String, String>} shaderParts - A collection of shader snippets.
-   * @param {Map<String, String>} defines - A collection of preprocessor macro definitions.
-   * @param {Map<String, Uniform>} uniforms - A collection of uniforms.
-   * @param {Camera} [camera=null] - A camera.
-   * @param {Boolean} [dithering=false] - Whether dithering should be enabled.
+   * @param shaderParts - A collection of shader snippets.
+   * @param defines - A collection of preprocessor macro definitions.
+   * @param uniforms - A collection of uniforms.
+   * @param camera - A camera.
+   * @param dithering - Whether dithering should be enabled.
    */
-
-  constructor(shaderParts, defines, uniforms, camera = null, dithering = false) {
+  constructor(
+    shaderParts: Map<Section, string>,
+    defines: Map<string, string>,
+    uniforms: Map<string, Uniform>,
+    camera: PerspectiveCamera | null = null,
+    dithering = false
+  ) {
 
     super({
-
-      type: 'EffectMaterial',
-
       defines: {
-
         DEPTH_PACKING: '0',
-
       },
-
       uniforms: {
-
         inputBuffer: new Uniform(null),
         depthBuffer: new Uniform(null),
 
@@ -47,56 +42,42 @@ export class EffectMaterial extends ShaderMaterial {
         cameraFar: new Uniform(1000.0),
         aspect: new Uniform(1.0),
         time: new Uniform(0.0),
-
       },
 
-      fragmentShader: fragmentTemplate.replace(Section.FRAGMENT_HEAD, shaderParts.get(Section.FRAGMENT_HEAD))
-        .replace(Section.FRAGMENT_MAIN_UV, shaderParts.get(Section.FRAGMENT_MAIN_UV))
-        .replace(Section.FRAGMENT_MAIN_IMAGE, shaderParts.get(Section.FRAGMENT_MAIN_IMAGE)),
+      fragmentShader: fragmentTemplate
+        .replace(Section.FRAGMENT_HEAD, shaderParts.get(Section.FRAGMENT_HEAD)!)
+        .replace(Section.FRAGMENT_MAIN_UV, shaderParts.get(Section.FRAGMENT_MAIN_UV)!)
+        .replace(Section.FRAGMENT_MAIN_IMAGE, shaderParts.get(Section.FRAGMENT_MAIN_IMAGE)!),
 
-      vertexShader: vertexTemplate.replace(Section.VERTEX_HEAD, shaderParts.get(Section.VERTEX_HEAD))
-        .replace(Section.VERTEX_MAIN_SUPPORT, shaderParts.get(Section.VERTEX_MAIN_SUPPORT)),
+      vertexShader: vertexTemplate
+        .replace(Section.VERTEX_HEAD, shaderParts.get(Section.VERTEX_HEAD)!)
+        .replace(Section.VERTEX_MAIN_SUPPORT, shaderParts.get(Section.VERTEX_MAIN_SUPPORT)!),
 
       dithering,
       depthWrite: false,
       depthTest: false,
-
     });
 
     if (defines !== null) {
-
       for (const entry of defines.entries()) {
-
         this.defines[entry[0]] = entry[1];
-
       }
-
     }
 
     if (uniforms !== null) {
-
       for (const entry of uniforms.entries()) {
-
         this.uniforms[entry[0]] = entry[1];
-
       }
-
     }
 
     this.adoptCameraSettings(camera);
-
   }
 
   /**
    * The current depth packing.
-   *
-   * @type {Number}
    */
-
-  get depthPacking() {
-
-    return Number.parseInt(this.defines.DEPTH_PACKING);
-
+  get depthPacking(): number {
+    return Number.parseInt(this.defines.DEPTH_PACKING, 10);
   }
 
   /**
@@ -106,59 +87,43 @@ export class EffectMaterial extends ShaderMaterial {
    * contains packed depth.
    *
    * You'll need to call {@link EffectPass#recompile} after changing this value.
-   *
-   * @type {Number}
    */
-
-  set depthPacking(value) {
-
+  set depthPacking(value: number) {
     this.defines.DEPTH_PACKING = value.toFixed(0);
-
   }
 
   /**
    * Sets the resolution.
    *
-   * @param {Number} width - The width.
-   * @param {Number} height - The height.
+   * @param width - The width.
+   * @param height - The height.
    */
-
-  setSize(width, height) {
-
+  setSize(
+    width: number,
+    height: number
+  ) {
     width = Math.max(width, 1.0);
     height = Math.max(height, 1.0);
 
     this.uniforms.resolution.value.set(width, height);
     this.uniforms.texelSize.value.set(1.0 / width, 1.0 / height);
     this.uniforms.aspect.value = width / height;
-
   }
 
   /**
    * Adopts the settings of the given camera.
-   *
-   * @param {Camera} [camera=null] - A camera.
    */
-
-  adoptCameraSettings(camera = null) {
-
+  adoptCameraSettings(camera: PerspectiveCamera | null = null) {
     if (camera !== null) {
-
-      this.uniforms.cameraNear.value = camera.near;
+      this.uniforms.cameraNear.value = camera && camera.near;
       this.uniforms.cameraFar.value = camera.far;
 
       if (camera instanceof PerspectiveCamera) {
-
         this.defines.PERSPECTIVE_CAMERA = '1';
-
-      } else {
-
-        delete this.defines.PERSPECTIVE_CAMERA;
-
       }
-
+      else {
+        delete this.defines.PERSPECTIVE_CAMERA;
+      }
     }
-
   }
-
 }
