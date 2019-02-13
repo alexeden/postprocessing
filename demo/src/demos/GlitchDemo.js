72e51ed8d28fa4f04550e481a42d302ae60705b2
Alex Eden <alexandereden91@gmail.com>
2019-02-13 01:16:21 -0600

    Tweak demo formatting

--- demo/src/demos/GlitchDemo.js
+++ demo/src/demos/GlitchDemo.js
@@ -1,3 +1,4 @@
+/* eslint-disable */
 import {
 	AmbientLight,
 	CubeTextureLoader,
@@ -35,38 +36,11 @@ export class GlitchDemo extends PostProcessingDemo {
 	 *
 	 * @param {EffectComposer} composer - An effect composer.
 	 */
-
 	constructor(composer) {
-
 		super("glitch", composer);
-
-		/**
-		 * An effect.
-		 *
-		 * @type {Effect}
-		 * @private
-		 */
-
 		this.effect = null;
-
-		/**
-		 * A pass.
-		 *
-		 * @type {Pass}
-		 * @private
-		 */
-
 		this.pass = null;
-
-		/**
-		 * An object.
-		 *
-		 * @type {Object3D}
-		 * @private
-		 */
-
 		this.object = null;
-
 	}
 
 	/**
@@ -74,9 +48,7 @@ export class GlitchDemo extends PostProcessingDemo {
 	 *
 	 * @return {Promise} A promise that will be fulfilled as soon as all assets have been loaded.
 	 */
-
 	load() {
-
 		const assets = this.assets;
 		const loadingManager = this.loadingManager;
 		const textureLoader = new TextureLoader(loadingManager);
@@ -91,64 +63,45 @@ export class GlitchDemo extends PostProcessingDemo {
 		];
 
 		return new Promise((resolve, reject) => {
-
 			if(assets.size === 0) {
-
 				loadingManager.onError = reject;
 				loadingManager.onProgress = (item, loaded, total) => {
-
 					if(loaded === total) {
-
 						resolve();
-
 					}
-
 				};
 
 				cubeTextureLoader.load(urls, function(textureCube) {
-
 					assets.set("sky", textureCube);
-
 				});
 
 				textureLoader.load("textures/perturb.jpg", function(texture) {
-
 					assets.set("perturbation-map", texture);
-
 				});
 
 				this.loadSMAAImages();
-
 			} else {
-
 				resolve();
-
 			}
-
 		});
-
 	}
 
 	/**
 	 * Creates the scene.
 	 */
-
 	initialize() {
-
 		const scene = this.scene;
 		const assets = this.assets;
 		const composer = this.composer;
 		const renderer = composer.renderer;
 
 		// Camera.
-
 		const camera = new PerspectiveCamera(50, window.innerWidth / window.innerHeight, 0.1, 2000);
 		camera.position.set(6, 1, 6);
 		camera.lookAt(scene.position);
 		this.camera = camera;
 
 		// Controls.
-
 		const controls = new DeltaControls(camera.position, camera.quaternion, renderer.domElement);
 		controls.settings.pointer.lock = false;
 		controls.settings.translation.enabled = false;
@@ -157,16 +110,13 @@ export class GlitchDemo extends PostProcessingDemo {
 		this.controls = controls;
 
 		// Fog.
-
 		scene.fog = new FogExp2(0x000000, 0.0025);
 		renderer.setClearColor(scene.fog.color);
 
 		// Sky.
-
 		scene.background = assets.get("sky");
 
 		// Lights.
-
 		const ambientLight = new AmbientLight(0x666666);
 		const directionalLight = new DirectionalLight(0xffbbaa);
 
@@ -177,7 +127,6 @@ export class GlitchDemo extends PostProcessingDemo {
 		scene.add(ambientLight);
 
 		// Random objects.
-
 		const object = new Object3D();
 		const geometry = new SphereBufferGeometry(1, 4, 4);
 
@@ -185,7 +134,6 @@ export class GlitchDemo extends PostProcessingDemo {
 		let i;
 
 		for(i = 0; i < 100; ++i) {
-
 			material = new MeshPhongMaterial({
 				color: 0xffffff * Math.random(),
 				flatShading: true
@@ -197,14 +145,12 @@ export class GlitchDemo extends PostProcessingDemo {
 			mesh.rotation.set(Math.random() * 2, Math.random() * 2, Math.random() * 2);
 			mesh.scale.multiplyScalar(Math.random());
 			object.add(mesh);
-
 		}
 
 		this.object = object;
 		scene.add(object);
 
 		// Passes.
-
 		const smaaEffect = new SMAAEffect(assets.get("smaa-search"), assets.get("smaa-area"));
 		smaaEffect.setEdgeDetectionThreshold(0.08);
 
@@ -233,7 +179,6 @@ export class GlitchDemo extends PostProcessingDemo {
 		composer.addPass(smaaPass);
 		composer.addPass(glitchPass);
 		composer.addPass(chromaticAberrationPass);
-
 	}
 
 	/**
@@ -241,9 +186,7 @@ export class GlitchDemo extends PostProcessingDemo {
 	 *
 	 * @param {Number} delta - The time since the last frame in seconds.
 	 */
-
 	render(delta) {
-
 		const object = this.object;
 		const twoPI = 2.0 * Math.PI;
 
@@ -251,19 +194,14 @@ export class GlitchDemo extends PostProcessingDemo {
 		object.rotation.y += 0.005;
 
 		if(object.rotation.x >= twoPI) {
-
 			object.rotation.x -= twoPI;
-
 		}
 
 		if(object.rotation.y >= twoPI) {
-
 			object.rotation.y -= twoPI;
-
 		}
 
 		super.render(delta);
-
 	}
 
 	/**
@@ -271,9 +209,7 @@ export class GlitchDemo extends PostProcessingDemo {
 	 *
 	 * @param {GUI} menu - A menu.
 	 */
-
 	registerOptions(menu) {
-
 		const effect = this.effect;
 		const perturbationMap = effect.getPerturbationMap();
 		const uniforms = effect.uniforms;
@@ -295,73 +231,47 @@ export class GlitchDemo extends PostProcessingDemo {
 		};
 
 		menu.add(params, "glitch mode", GlitchMode).onChange(() => {
-
 			effect.mode = Number.parseInt(params["glitch mode"]);
-
 		});
 
 		menu.add(params, "custom pattern").onChange(() => {
-
 			if(params["custom pattern"]) {
-
 				effect.setPerturbationMap(perturbationMap);
-
 			} else {
-
 				effect.setPerturbationMap(effect.generatePerturbationMap(64));
-
 			}
-
 		});
 
 		menu.add(params, "min delay").min(0.0).max(2.0).step(0.001).onChange(() => {
-
 			delay.x = params["min delay"];
-
 		});
 
 		menu.add(params, "max delay").min(2.0).max(4.0).step(0.001).onChange(() => {
-
 			delay.y = params["max delay"];
-
 		});
 
 		menu.add(params, "min duration").min(0.0).max(0.6).step(0.001).onChange(() => {
-
 			duration.x = params["min duration"];
-
 		});
 
 		menu.add(params, "max duration").min(0.6).max(1.8).step(0.001).onChange(() => {
-
 			duration.y = params["max duration"];
-
 		});
 
 		menu.add(params, "weak glitches").min(0.0).max(1.0).step(0.001).onChange(() => {
-
 			strength.x = params["weak glitches"];
-
 		});
 
 		menu.add(params, "strong glitches").min(0.0).max(1.0).step(0.001).onChange(() => {
-
 			strength.y = params["strong glitches"];
-
 		});
 
 		menu.add(params, "glitch ratio").min(0.0).max(1.0).step(0.001).onChange(() => {
-
 			effect.ratio = Number.parseFloat(params["glitch ratio"]);
-
 		});
 
 		menu.add(params, "columns").min(0.0).max(0.5).step(0.001).onChange(() => {
-
 			uniforms.get("columns").value = params.columns;
-
 		});
-
 	}
-
 }
