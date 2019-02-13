aa18661ad2cd894701c542773e4dc64c8b7ff134
Alex Eden <alexandereden91@gmail.com>
2019-02-12 23:56:58 -0600

    Add typescript rollup plugin to get demos to compile

--- rollup.config.js
+++ rollup.config.js
@@ -1,81 +1,28 @@
 import babel from "rollup-plugin-babel";
-import minify from "rollup-plugin-babel-minify";
+import typescript from "rollup-plugin-typescript2";
 import commonjs from "rollup-plugin-commonjs";
 import resolve from "rollup-plugin-node-resolve";
 import string from "rollup-plugin-string";
 
-const pkg = require("./package.json");
-const date = (new Date()).toDateString();
-
-const banner = `/**
- * ${pkg.name} v${pkg.version} build ${date}
- * ${pkg.homepage}
- * Copyright ${date.slice(-4)} ${pkg.author.name}, ${pkg.license}
- */`;
-
-const lib = {
-
-	input: pkg.module,
-	output: {
-		file: "build/" + pkg.name + ".js",
-		format: "umd",
-		name: pkg.name.replace(/-/g, "").toUpperCase(),
-		banner: banner,
-		globals: { three: "THREE" }
-	},
-
-	external: ["three"],
-	plugins: [resolve(), string({
-		include: ["**/*.frag", "**/*.vert"]
-	})].concat(process.env.NODE_ENV === "production" ? [babel()] : [])
-
-};
-
-const demo = {
-
+export default [{
 	input: "demo/src/index.js",
 	output: {
 		file: "public/demo/index.js",
 		format: "iife",
 		globals: { three: "THREE" }
 	},
-
 	external: ["three"],
-	plugins: [resolve(), commonjs(), string({
-		include: ["**/*.frag", "**/*.vert"]
-	})].concat(process.env.NODE_ENV === "production" ? [babel()] : [])
-
-};
-
-export default [lib, demo].concat((process.env.NODE_ENV === "production") ? [
-
-	Object.assign({}, lib, {
-
-		output: Object.assign({}, lib.output, {
-			file: "build/" + pkg.name + ".min.js"
+	plugins: [
+		resolve(),
+		typescript({
+			tsconfig: "./src/tsconfig.json",
+			tsconfigOverride: {
+				declaration: false
+			}
 		}),
-
-		plugins: [resolve(), string({
+		commonjs({ extensions: [".js", ".ts"] }),
+		string({
 			include: ["**/*.frag", "**/*.vert"]
-		})].concat([babel(), minify({
-			bannerNewLine: true,
-			comments: false
-		})])
-
-	}),
-
-	Object.assign({}, demo, {
-
-		output: Object.assign({}, demo.output, {
-			file: "public/demo/index.min.js"
-		}),
-
-		plugins: [resolve(), commonjs(), string({
-			include: ["**/*.frag", "**/*.vert"]
-		})].concat([babel(), minify({
-			comments: false
-		})])
-
-	})
-
-] : []);
+		})
+	].concat(process.env.NODE_ENV === "production" ? [babel()] : [])
+}];
