8002956e1c5d5452622f7ac62e636b7351b5bf0e
Alex Eden <alexandereden91@gmail.com>
2019-10-22 19:34:50 -0500

    Start converting ConvolutionMaterial

---
+++ src/materials/convolution/ConvolutionMaterial.ts
@@ -0,0 +1,114 @@
+import { ShaderMaterial, Uniform, Vector2 } from 'three';
+
+import fragment from './convolution.frag';
+import vertex from './convolution.vert';
+
+
+/**
+ * A kernel size enumeration.
+ */
+export enum KernelSize {
+  /** A very small kernel that matches a 7x7 Gauss blur kernel. */
+  VERY_SMALL = 0,
+  /** A small kernel that matches a 15x15 Gauss blur kernel. */
+  SMALL = 1,
+  /** A medium sized kernel that matches a 23x23 Gauss blur kernel. */
+  MEDIUM = 2,
+  /** A large kernel that matches a 35x35 Gauss blur kernel. */
+  LARGE = 3,
+  /** A very large kernel that matches a 63x63 Gauss blur kernel. */
+  VERY_LARGE = 4,
+  /** A huge kernel that matches a 127x127 Gauss blur kernel. */
+  HUGE = 5,
+}
+
+/**
+ * The Kawase blur kernel presets.
+ */
+const kernelPresets = [
+  new Float32Array([0.0, 0.0]),
+  new Float32Array([0.0, 1.0, 1.0]),
+  new Float32Array([0.0, 1.0, 1.0, 2.0]),
+  new Float32Array([0.0, 1.0, 2.0, 2.0, 3.0]),
+  new Float32Array([0.0, 1.0, 2.0, 3.0, 4.0, 4.0, 5.0]),
+  new Float32Array([0.0, 1.0, 2.0, 3.0, 4.0, 5.0, 7.0, 8.0, 9.0, 10.0]),
+];
+
+
+/**
+ * An optimised convolution shader material.
+ *
+ * This material supports dithering.
+ *
+ * Based on the GDC2003 Presentation by Masaki Kawase, Bunkasha Games:
+ *  Frame Buffer Postprocessing Effects in DOUBLE-S.T.E.A.L (Wreckless)
+ * and an article by Filip Strugar, Intel:
+ *  An investigation of fast real-time GPU-based image blur algorithms
+ *
+ * Further modified according to Apple's
+ * [Best Practices for Shaders](https://goo.gl/lmRoM5).
+ */
+export class ConvolutionMaterial extends ShaderMaterial {
+  /**
+   * Constructs a new convolution material.
+   *
+   * @param [texelSize] - The absolute screen texel size.
+   */
+  constructor(texelSize = new Vector2()) {
+
+    super({
+      uniforms: {
+        inputBuffer: new Uniform(null),
+        texelSize: new Uniform(new Vector2()),
+        halfTexelSize: new Uniform(new Vector2()),
+        kernel: new Uniform(0.0),
+
+      },
+
+      fragmentShader: fragment,
+      vertexShader: vertex,
+
+      depthWrite: false,
+      depthTest: false,
+
+    });
+
+    this.setTexelSize(texelSize.x, texelSize.y);
+
+    /**
+     * The current kernel size.
+     *
+     * @type {KernelSize}
+     */
+
+    this.kernelSize = KernelSize.LARGE;
+
+  }
+
+  /**
+   * Returns the kernel.
+   *
+   * @return {Float32Array} The kernel.
+   */
+
+  getKernel() {
+
+    return kernelPresets[this.kernelSize];
+
+  }
+
+  /**
+   * Sets the texel size.
+   *
+   * @param {Number} x - The texel width.
+   * @param {Number} y - The texel height.
+   */
+
+  setTexelSize(x, y) {
+
+    this.uniforms.texelSize.value.set(x, y);
+    this.uniforms.halfTexelSize.value.set(x, y).multiplyScalar(0.5);
+
+  }
+
+}
--- src/materials/index.ts
+++ src/materials/index.ts
@@ -1,6 +1,6 @@
 // export { AdaptiveLuminanceMaterial } from './AdaptiveLuminanceMaterial';
 import { ColorEdgesMaterial } from './color-edges/ColorEdgesMaterial';
-// export { ConvolutionMaterial, KernelSize } from './ConvolutionMaterial';
+import { ConvolutionMaterial, KernelSize } from './convolution/ConvolutionMaterial';
 import { CopyMaterial } from './copy/CopyMaterial';
 // export { DepthComparisonMaterial } from './DepthComparisonMaterial';
 import { EffectMaterial } from './effect/EffectMaterial';
@@ -11,31 +11,16 @@ import { SMAAWeightsMaterial } from './smaa-weights/SMAAWeightsMaterial';
 
 export type PostprocessingMaterial
   = ColorEdgesMaterial
+  | ConvolutionMaterial
   | CopyMaterial
   | EffectMaterial
   | SMAAWeightsMaterial;
 
-/**
- * A kernel size enumeration.
- */
-export enum KernelSize {
-  /** A very small kernel that matches a 7x7 Gauss blur kernel. */
-  VERY_SMALL = 0,
-  /** A small kernel that matches a 15x15 Gauss blur kernel. */
-  SMALL = 1,
-  /** A medium sized kernel that matches a 23x23 Gauss blur kernel. */
-  MEDIUM = 2,
-  /** A large kernel that matches a 35x35 Gauss blur kernel. */
-  LARGE = 3,
-  /** A very large kernel that matches a 63x63 Gauss blur kernel. */
-  VERY_LARGE = 4,
-  /** A huge kernel that matches a 127x127 Gauss blur kernel. */
-  HUGE = 5,
-}
-
 export {
   ColorEdgesMaterial,
+  ConvolutionMaterial,
   CopyMaterial,
   EffectMaterial,
+  KernelSize,
   SMAAWeightsMaterial,
 };
