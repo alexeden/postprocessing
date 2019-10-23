d73692225fa93b8882ca1319974754a206c68570
Alex Eden <alexandereden91@gmail.com>
2019-10-22 20:23:31 -0500

    Install nullish-coalescing operator plugin for babel

--- babel.config.js
+++ babel.config.js
@@ -3,7 +3,10 @@ module.exports = function(api) {
 	api.cache.forever();
 
 	return {
-		comments: false,
+    comments: false,
+    plugins: [
+      '@babel/plugin-proposal-nullish-coalescing-operator',
+    ],
 		presets: [
 			["@babel/preset-env", {
 				"modules": false
