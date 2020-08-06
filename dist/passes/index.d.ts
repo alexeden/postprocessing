10df413977deb708292162bb11e77eb00a734d74
Alex Eden <alexandereden91@gmail.com>
2020-08-05 20:58:55 -0500

    Forgot to include NormalPass in exports

--- dist/passes/index.d.ts
+++ dist/passes/index.d.ts
@@ -2,6 +2,7 @@ export { BlurPass } from './BlurPass';
 export { ClearMaskPass } from './ClearMaskPass';
 export { ClearPass } from './ClearPass';
 export { MaskPass } from './MaskPass';
+export { NormalPass } from './NormalPass';
 export { RenderPass } from './RenderPass';
 export { SavePass } from './SavePass';
 export { ShaderPass } from './ShaderPass';
--- src/passes/index.ts
+++ src/passes/index.ts
@@ -3,7 +3,7 @@ export { ClearMaskPass } from './ClearMaskPass';
 export { ClearPass } from './ClearPass';
 // export { DepthPass } from './DepthPass';
 export { MaskPass } from './MaskPass';
-// export { NormalPass } from './NormalPass';
+export { NormalPass } from './NormalPass';
 export { RenderPass } from './RenderPass';
 export { SavePass } from './SavePass';
 export { ShaderPass } from './ShaderPass';
